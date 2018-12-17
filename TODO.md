dc911da94f57637f2f1ef5527671ae7cc1f5eb00
Dominik Sigmund <dominik.sigmund@br.de>
2018-12-17 15:37:22 +0100

    Many

--- TODO.md
+++ TODO.md
@@ -1,26 +1,21 @@
 # TODOS
 
+## Allgemeines
+
 * Schrifttyp: Im Moment noch sehr nüchtern. Lass uns mal probieren, ob wir eine freundlichere, rundere Schrift finden.
-* BlauTon: #075c98
-* Überschrift „Starke Geschichten..“ bitte in Blau
-* Die Überschrift in eine Zeile setzen. Dafür die Logos kleiner machen.
-* Achtung, im Moment sind Überschrift und Reiter darunter nicht bündig.
-* Blauer Block: Schrift bitte um einiges kleiner machen. Ersten Satz absetzen. Kleiner machen (Bild kleiner)
+* Filme: Beschreibung dazu
+
+## Big
+
+? * Die Überschrift in eine Zeile setzen. Dafür die Logos kleiner machen.
 * Titelbild: slideshow (stills, titel und land, onClick Play and stop slideshow)
-* Film-Stills tauschen.
-* Weltkarte: Fähnchen Weg. on Hover: show Popup with still, on click video, titel, land
-* Impressum: Bitte rausnehmen „Ein Projekt des IZI“. Ist ja nicht nur IZI Projekt, sondern hat 3 „Mütter“.
-* Mobil: IZI-Logo kleiner (nach höhe, nicht breite)
-* Liste: Sendungstitel dazu
+* Weltkarte: on Hover: show Popup with still, on click video, titel, land
 
-## Übersicht Filmtitel und Land
+## Reiter
 
-1. Isa und der Kirschbaum (Deutschland)
-2. Der Tag, an dem ich zum großen Bruder wurde (Ägypten)
-3. Bitte nicht Rosa! (Norwegen)
-4. Geschlagen (Libanon)
-5. Ballspielen verboten!  (Thailand)
-6. Wenn Eltern immer nur streiten (Taiwan)
-7. Sleepy, das Küken (Mongolei)
-8. Allein im Busch (Namibia)
-9. Stromausfall (Argentinien)
+* Partner
+* Die Filme
+
+## Fehler
+
+* Still zu Alleine im Busch fehlt
--- js/index.js
+++ js/index.js
@@ -28,29 +28,41 @@ $(document).ready(function () {
       click: function (country) {
         loadVideo(country)
       },
-      disableCountries: ['af', 'ax', 'al', 'dz', 'as', 'ad', 'ao', 'ai', 'aq', 'ag', 'ar', 'am', 'aw', 'au', 'at', 'az', 'bs', 'bh', 'bd', 'bb', 'by', 'be', 'bz', 'bj', 'bm', 'bt', 'bo', 'ba', 'bw', 'bv', 'br', 'io', 'bn', 'bg', 'bf', 'bi', 'kh', 'cm', 'ca', 'cv', 'ky', 'cf', 'td', 'cl', 'cn', 'cx', 'cc', 'co', 'km', 'cg', 'cd', 'ck', 'cr', 'ci', 'hr', 'cu', 'cy', 'cz', 'dk', 'dj', 'dm', 'do', 'ec', 'eg', 'sv', 'gq', 'er', 'ee', 'et', 'fk', 'fo', 'fj', 'fi', 'fr', 'gf', 'pf', 'tf', 'ga', 'gm', 'ge', 'gh', 'gi', 'gr', 'gl', 'gd', 'gp', 'gu', 'gt', 'gg', 'gn', 'gw', 'gy', 'ht', 'hm', 'va', 'hn', 'hk', 'hu', 'is', 'in', 'id', 'ir', 'iq', 'ie', 'im', 'il', 'it', 'jm', 'jp', 'je', 'jo', 'kz', 'ke', 'ki', 'kr', 'kw', 'kg', 'la', 'lv', 'lb', 'ls', 'lr', 'ly', 'li', 'lt', 'lu', 'mo', 'mk', 'mg', 'mw', 'my', 'mv', 'ml', 'mt', 'mh', 'mq', 'mr', 'mu', 'yt', 'mx', 'fm', 'md', 'mc', 'mn', 'me', 'ms', 'ma', 'mz', 'mm', 'nr', 'np', 'nl', 'an', 'nc', 'nz', 'ni', 'ne', 'ng', 'nu', 'nf', 'mp', 'no', 'om', 'pk', 'pw', 'ps', 'pa', 'pg', 'py', 'pe', 'ph', 'pn', 'pl', 'pt', 'pr', 'qa', 're', 'ro', 'ru', 'rw', 'bl', 'sh', 'kn', 'lc', 'mf', 'pm', 'vc', 'ws', 'sm', 'st', 'sa', 'sn', 'sw', 'rs', 'sc', 'sl', 'sg', 'sk', 'si', 'sb', 'so', 'za', 'gs', 'es', 'lk', 'sd', 'sr', 'sj', 'sz', 'se', 'ch', 'sy', 'tw', 'tj', 'tz', 'tl', 'tg', 'tk', 'to', 'tt', 'tn', 'tr', 'tm', 'tc', 'tv', 'ug', 'ua', 'ae', 'gb', 'us', 'um', 'uy', 'uz', 'vu', 've', 'vn', 'vg', 'vi', 'wf', 'eh', 'ye', 'zm', 'zw'],
+      disableCountries: ['af', 'ax', 'al', 'dz', 'as', 'ad', 'ao', 'ai', 'aq', 'ag', 'am', 'aw', 'au', 'at', 'az', 'bs', 'bh', 'bd', 'bb', 'by', 'be', 'bz', 'bj', 'bm', 'bt', 'bo', 'ba', 'bw', 'bv', 'br', 'io', 'bn', 'bg', 'bf', 'bi', 'kh', 'cm', 'ca', 'cv', 'ky', 'cf', 'td', 'cl', 'cn', 'cx', 'cc', 'co', 'km', 'cg', 'cd', 'ck', 'cr', 'ci', 'hr', 'cu', 'cy', 'cz', 'dk', 'dj', 'dm', 'do', 'ec', 'sv', 'gq', 'er', 'ee', 'et', 'fk', 'fo', 'fj', 'fi', 'fr', 'gf', 'pf', 'tf', 'ga', 'gm', 'ge', 'gh', 'gi', 'gr', 'gl', 'gd', 'gp', 'gu', 'gt', 'gg', 'gn', 'gw', 'gy', 'ht', 'hm', 'va', 'hn', 'hk', 'hu', 'is', 'in', 'id', 'ir', 'iq', 'ie', 'im', 'il', 'it', 'jm', 'jp', 'je', 'jo', 'kz', 'ke', 'ki', 'kr', 'kw', 'kg', 'la', 'lv', 'ls', 'lr', 'ly', 'li', 'lt', 'lu', 'mo', 'mk', 'mg', 'mw', 'my', 'mv', 'ml', 'mt', 'mh', 'mq', 'mr', 'mu', 'yt', 'mx', 'fm', 'md', 'mc', 'me', 'ms', 'ma', 'mz', 'mm', 'nr', 'np', 'nl', 'an', 'nc', 'nz', 'ni', 'ne', 'ng', 'nu', 'nf', 'mp', 'om', 'pk', 'pw', 'ps', 'pa', 'pg', 'py', 'pe', 'ph', 'pn', 'pl', 'pt', 'pr', 'qa', 're', 'ro', 'ru', 'rw', 'bl', 'sh', 'kn', 'lc', 'mf', 'pm', 'vc', 'ws', 'sm', 'st', 'sa', 'sn', 'sw', 'rs', 'sc', 'sl', 'sg', 'sk', 'si', 'sb', 'so', 'za', 'gs', 'es', 'lk', 'sd', 'sr', 'sj', 'sz', 'se', 'ch', 'sy', 'tj', 'tz', 'tl', 'tg', 'tk', 'to', 'tt', 'tn', 'tr', 'tm', 'tc', 'tv', 'ug', 'ua', 'ae', 'gb', 'us', 'um', 'uy', 'uz', 'vu', 've', 'vn', 'vg', 'vi', 'wf', 'eh', 'ye', 'zm', 'zw'],
       individualCountrySettings: [
-        {name: 'de', color: '#1e90ff'},
-        {name: 'th', color: '#1e90ff'},
-        {name: 'na', color: '#1e90ff'}
+        {name: 'de', color: '#075c98'},
+        {name: 'eg', color: '#075c98'},
+        {name: 'no', color: '#075c98'},
+        {name: 'lb', color: '#075c98'},
+        {name: 'th', color: '#075c98'},
+        {name: 'tw', color: '#075c98'},
+        {name: 'mn', color: '#075c98'},
+        {name: 'na', color: '#075c98'},
+        {name: 'ar', color: '#075c98'}
       ]
     }, function () {
-      setFlag('de')
-      setFlag('th')
-      setFlag('na')
+      // setFlag('de')
+      // setFlag('th')
+      // setFlag('na')
     })
   }
 })
-function setFlag (country) {
+/* function setFlag (country) {
   var pos = $('svg path#' + country + ', svg text#' + country + ', svg rect#' + country).offset()
   $('body').last().after('<img data-country="' + country + '" src="images/flag-' + country + '.png" style="left:' + pos.left + 'px;top:' + pos.top + 'px" class="flag-marker" onclick="loadVideo(\'' + country + '\')"/>')
-}
+} */
 function loadVideo (country) {
   let video = ''
   switch (country) {
-    case 'de': video = 'videos/1_Kirschbaum.mp4'; break
-    case 'th': video = 'videos/11_Elternstreit.mp4'; break
-    case 'na': video = 'videos/8_Allein_im_Busch.mp4'; break
+    case 'de': video = 'videos/1_Kirschbaum_Deutschland.mp4'; break
+    case 'eg': video = 'videos/2_Grosser_Bruder_Aegypten.mp4'; break
+    case 'no': video = 'videos/3_Nicht_Rosa_Norwegen.mp4'; break
+    case 'lb': video = 'videos/4-Geschlagen_Syrien.mp4'; break
+    case 'th': video = 'videos/7_Ballspielen_verboten_Thailand.mp4'; break
+    case 'tw': video = 'videos/11_Elternstreit_Taiwan.mp4'; break
+    case 'mn': video = 'videos/9-Sleepy_Mongolei.mp4'; break
+    case 'na': video = 'videos/8_Allein_im_Busch_Namibia.mp4'; break
+    case 'ar': video = 'videos/13-Stromausfall_Argentinien.mp4'; break
     default: video = ''; break
   }
   if (video !== '') {
