552ea06081e2d1f80ad8f2e6642266e06d5ff2c8
Dominik Sigmund <dominik.sigmund@br.de>
2019-01-11 11:04:42 +0100

    Added more Stories

--- TODO.md
+++ TODO.md
@@ -8,19 +8,6 @@
 
 ## Filme
 
-### Was mich das Meer lehrte, Kuba
-
-5 min
-Der 11-jährige Alejandro hat eine intensive Verbindung zum Meer. Er surft jeden Tag und liebt es, sich am Brett neuen Herausforderungen zu stellen. In dem Film erzählt er uns von einer Lektion, die er durch das Meer gelernt hat: Schwimm nicht gegen die Wellen, sei schlau, tauch unter ihnen durch. Nutze die Kraft des Wassers und mach deinen eigenen „move“. Dass das Meer auch gefährlich sein kann, merkt Alejandro, als seine Surfleine sich an einer Koralle verfängt und die Wellen ihn unter Wasser drücken. In diesem Augenblick erkennt er seine besondere Stärke und findet seinen Weg zurück an die Oberfläche.
-
-### Der Tag, als ich den Richtigen erwischte, Mongolei
-
-5 min
-Zaya kommt aus einem kleinen Dorf aus den Bergen. Als der Wechsel in die 5. Klasse ansteht, muss sie in die Stadt umziehen. Ihre neue Lehrerin weist zwar die Kinder in Zayas neuer Klasse an, nett zu ihr zu sein, sie leider sind sie es nicht. Sie machen sich über ihre Frisur lustig und ziehen sie an den Haaren. Zaya schämt sich und weiß nicht, was sie machen soll. Als sie am nächsten Tag wieder in die Schule kommt und das Ärgern wieder losgeht, trifft sie eine Entscheidung. Sie will sich wehren. Ob sie es schafft?
-
-### Ausgrbrochen, Norwegen
-
-Jovann und J.P. gehen jeden Tag in den Kinderhort. Doch alles was ihnen Spaß macht, wie etwa mit Stöcken zu kämpfen, ist verboten. Eines Tages beschließen sie, etwas ganz Verbotenes zu tun: Sie hauen ab. Sie genießen die Freiheit, rutschen lange Schneerutschen herunter, doch obwohl sie versuchen, vorsichtig zu sein, verletzt J.P. sich. Sie beschließen, zu J.P.s Haus zu gehen, das ein ganzes Stück weiter unten am zugefrorenen Fluss liegt. Doch der Weg ist viel weiter als gedacht und langsam wird es dunkel. Da kommt ein Auto und Jovanns Tante ruft nach ihnen. Sollen sie aufgeben und sich von einem Erwachsenen retten lassen?
 
 ### Mein erstes Omelette, Vietnam
 
--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
         // loadVideo(country)
         scrollTo(country)
       },
-      disableCountries: ['af', 'ax', 'al', 'dz', 'as', 'ad', 'ao', 'ai', 'aq', 'ag', 'am', 'aw', 'au', 'at', 'az', 'bs', 'bh', 'bd', 'bb', 'by', 'be', 'bz', 'bj', 'bm', 'bt', 'bo', 'ba', 'bw', 'bv', 'br', 'io', 'bn', 'bg', 'bf', 'bi', 'kh', 'cm', 'ca', 'cv', 'ky', 'cf', 'td', 'cl', 'cn', 'cx', 'cc', 'co', 'km', 'cg', 'cd', 'ck', 'cr', 'ci', 'hr', 'cy', 'cz', 'dk', 'dj', 'dm', 'do', 'ec', 'sv', 'gq', 'er', 'ee', 'et', 'fk', 'fo', 'fj', 'fi', 'fr', 'gf', 'pf', 'tf', 'ga', 'gm', 'ge', 'gh', 'gi', 'gr', 'gl', 'gd', 'gp', 'gu', 'gt', 'gg', 'gn', 'gw', 'gy', 'ht', 'hm', 'va', 'hn', 'hk', 'hu', 'is', 'in', 'id', 'ir', 'iq', 'ie', 'im', 'il', 'it', 'jm', 'jp', 'je', 'jo', 'kz', 'ke', 'ki', 'kr', 'kw', 'kg', 'la', 'lv', 'ls', 'lr', 'ly', 'li', 'lt', 'lu', 'mo', 'mk', 'mg', 'mw', 'my', 'mv', 'ml', 'mt', 'mh', 'mq', 'mr', 'mu', 'yt', 'mx', 'fm', 'md', 'mc', 'me', 'ms', 'ma', 'mz', 'mm', 'nr', 'np', 'nl', 'an', 'nc', 'nz', 'ni', 'ne', 'ng', 'nu', 'nf', 'mp', 'om', 'pk', 'pw', 'ps', 'pa', 'pg', 'py', 'pe', 'ph', 'pn', 'pl', 'pt', 'pr', 'qa', 're', 'ro', 'ru', 'rw', 'bl', 'sh', 'kn', 'lc', 'mf', 'pm', 'vc', 'ws', 'sm', 'st', 'sa', 'sn', 'sw', 'rs', 'sc', 'sl', 'sg', 'sk', 'si', 'sb', 'so', 'za', 'gs', 'es', 'lk', 'sd', 'sr', 'sj', 'sz', 'se', 'ch', 'sy', 'tj', 'tz', 'tl', 'tg', 'tk', 'to', 'tt', 'tn', 'tr', 'tm', 'tc', 'tv', 'ug', 'ua', 'ae', 'gb', 'us', 'um', 'uy', 'uz', 'vu', 've', 'vn', 'vg', 'vi', 'wf', 'eh', 'ye', 'zm', 'zw'],
+      disableCountries: ['af', 'ax', 'al', 'dz', 'as', 'ad', 'ao', 'ai', 'aq', 'ag', 'am', 'aw', 'au', 'at', 'az', 'bs', 'bh', 'bd', 'bb', 'by', 'be', 'bz', 'bj', 'bm', 'bt', 'bo', 'ba', 'bw', 'bv', 'br', 'io', 'bn', 'bg', 'bf', 'bi', 'kh', 'cm', 'ca', 'cv', 'ky', 'cf', 'td', 'cl', 'cn', 'cx', 'cc', 'co', 'km', 'cg', 'cd', 'ck', 'cr', 'ci', 'hr', 'cy', 'cz', 'dk', 'dj', 'dm', 'do', 'ec', 'sv', 'gq', 'er', 'ee', 'et', 'fk', 'fo', 'fj', 'fi', 'fr', 'gf', 'pf', 'tf', 'ga', 'gm', 'ge', 'gh', 'gi', 'gr', 'gl', 'gd', 'gp', 'gu', 'gt', 'gg', 'gn', 'gw', 'gy', 'ht', 'hm', 'va', 'hn', 'hk', 'hu', 'is', 'in', 'id', 'ir', 'iq', 'ie', 'im', 'il', 'it', 'jm', 'jp', 'je', 'jo', 'kz', 'ke', 'ki', 'kr', 'kw', 'kg', 'la', 'lv', 'ls', 'lr', 'ly', 'li', 'lt', 'lu', 'mo', 'mk', 'mg', 'mw', 'my', 'mv', 'ml', 'mt', 'mh', 'mq', 'mr', 'mu', 'yt', 'mx', 'fm', 'md', 'mc', 'me', 'ms', 'ma', 'mz', 'mm', 'nr', 'np', 'nl', 'an', 'nc', 'nz', 'ni', 'ne', 'ng', 'nu', 'nf', 'mp', 'om', 'pk', 'pw', 'ps', 'pa', 'pg', 'py', 'pe', 'ph', 'pn', 'pl', 'pt', 'pr', 'qa', 're', 'ro', 'ru', 'rw', 'bl', 'sh', 'kn', 'lc', 'mf', 'pm', 'vc', 'ws', 'sm', 'st', 'sa', 'sn', 'sw', 'rs', 'sc', 'sl', 'sg', 'sk', 'si', 'sb', 'so', 'za', 'gs', 'es', 'lk', 'sd', 'sr', 'sj', 'sz', 'se', 'ch', 'sy', 'tj', 'tz', 'tl', 'tg', 'tk', 'to', 'tt', 'tn', 'tr', 'tm', 'tc', 'tv', 'ug', 'ua', 'ae', 'gb', 'us', 'um', 'uy', 'uz', 'vu', 've', 'vg', 'vi', 'wf', 'eh', 'ye', 'zm', 'zw'],
       individualCountrySettings: [
         {name: 'de', color: '#075c98'},
         {name: 'eg', color: '#075c98'},
@@ -59,7 +59,8 @@ $(document).ready(function () {
         {name: 'mn', color: '#075c98'},
         {name: 'na', color: '#075c98'},
         {name: 'ar', color: '#075c98'},
-        {name: 'cu', color: '#075c98'}
+        {name: 'cu', color: '#075c98'},
+        {name: 'vn', color: '#075c98'}
       ]
     }, function () {
       // setFlag('de')
@@ -106,6 +107,7 @@ function scrollTo (country) {
     case 'na': element = 'f-allein_im_busch'; break
     case 'ar': element = 'f-stromausfall'; break
     case 'cu': element = 'f-meer'; break
+    case 'vn': element = 'f-omelette'; break
     default: element = ''; break
   }
   if (element !== '') {
