efd1116489e09bdc644ccc8c321dc6c9636b9f50
Dominik Sigmund <dominik.sigmund@br.de>
2019-01-21 16:19:50 +0100

    resort

--- TODO.md
+++ TODO.md
@@ -7,31 +7,3 @@
 * Auch zu starke Diskrepanz zwischen Größe der Überschrift(en) und Größer der Textschrift, das sollten wir ausgewogener darstellen.
 
 * Cards: 3 per Column
-
-
-* Episoden Reihenfolge:
-1. Isa und der Kirschbaum, Deutschland
-2. Der Tag, an dem ich zum großen Bruder wurde, Ägypten
-3. Bitte nicht Rosa!, Norwegen
-4. Geschlagen, Libanon
-5. Was mich das Meer lehrte, Kuba
-6. Der Tag, als ich den Richtigen erwischte, Mongolei
-7. Ballspielen verboten!, Thailand
-8. Allein im Busch, Namibia
-9. Sleepy, das Küken, Mongolei
-10. Ausgebrochen, Norwegen
-11. Wenn Eltern immer nur streiten, Taiwan
-12. Mein erstes Omelette, Vietnam
-13. Stromausfall, Argentinien
-
-
-* Beim klicken auf Karte oder slideshow: set history (für backbutton)
-
-
-* Neue Länder, Hover "Coming Soon"
-- Ecuador
-- Kolumbien
-- Mexiko
-
-
- 
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/* global $ */
+/* global $, history, alert */
 var slideIndex = 1
 $(document).ready(function () {
   setInterval(function () {
@@ -6,6 +6,7 @@ $(document).ready(function () {
   }, 5000)
   showSlides(slideIndex)
   $('#up').on('click', function () {
+    history.pushState({}, 'Starke Geschichten', '#')
     $('html, body').animate({scrollTop: ($('#masthead').offset().top)}, 'slow')
   })
   $('.card-description-link').on('click', function () {
@@ -18,9 +19,11 @@ $(document).ready(function () {
     $(this).parent().find('.video-thumbnail').hide()
   })
   $('.slideshow-slide').on('click', function () {
+    history.pushState({}, 'Starke Geschichten', '#' + $(this).find('img').data('scrollto'))
     $('html, body').animate({scrollTop: ($('#' + $(this).find('img').data('scrollto')).offset().top)}, 'slow')
   })
   $('.mobile-nav-button').on('click', function () {
+    history.pushState({}, 'Starke Geschichten', '#' + $(this).data('scrollto'))
     $('html, body').animate({scrollTop: ($('#' + $(this).data('scrollto')).offset().top)}, 'slow')
   })
   $('.video-thumbnail').on('click', function () {
@@ -48,10 +51,9 @@ $(document).ready(function () {
       hoverColor: '#BB0029',
       backgroundColor: 'white',
       click: function (country) {
-        // loadVideo(country)
         scrollTo(country)
       },
-      disableCountries: ['af', 'ax', 'al', 'dz', 'as', 'ad', 'ao', 'ai', 'aq', 'ag', 'am', 'aw', 'au', 'at', 'az', 'bs', 'bh', 'bd', 'bb', 'by', 'be', 'bz', 'bj', 'bm', 'bt', 'bo', 'ba', 'bw', 'bv', 'br', 'io', 'bn', 'bg', 'bf', 'bi', 'kh', 'cm', 'ca', 'cv', 'ky', 'cf', 'td', 'cl', 'cn', 'cx', 'cc', 'co', 'km', 'cg', 'cd', 'ck', 'cr', 'ci', 'hr', 'cy', 'cz', 'dk', 'dj', 'dm', 'do', 'ec', 'sv', 'gq', 'er', 'ee', 'et', 'fk', 'fo', 'fj', 'fi', 'fr', 'gf', 'pf', 'tf', 'ga', 'gm', 'ge', 'gh', 'gi', 'gr', 'gl', 'gd', 'gp', 'gu', 'gt', 'gg', 'gn', 'gw', 'gy', 'ht', 'hm', 'va', 'hn', 'hk', 'hu', 'is', 'in', 'id', 'ir', 'iq', 'ie', 'im', 'il', 'it', 'jm', 'jp', 'je', 'jo', 'kz', 'ke', 'ki', 'kr', 'kw', 'kg', 'la', 'lv', 'ls', 'lr', 'ly', 'li', 'lt', 'lu', 'mo', 'mk', 'mg', 'mw', 'my', 'mv', 'ml', 'mt', 'mh', 'mq', 'mr', 'mu', 'yt', 'mx', 'fm', 'md', 'mc', 'me', 'ms', 'ma', 'mz', 'mm', 'nr', 'np', 'nl', 'an', 'nc', 'nz', 'ni', 'ne', 'ng', 'nu', 'nf', 'mp', 'om', 'pk', 'pw', 'ps', 'pa', 'pg', 'py', 'pe', 'ph', 'pn', 'pl', 'pt', 'pr', 'qa', 're', 'ro', 'ru', 'rw', 'bl', 'sh', 'kn', 'lc', 'mf', 'pm', 'vc', 'ws', 'sm', 'st', 'sa', 'sn', 'sw', 'rs', 'sc', 'sl', 'sg', 'sk', 'si', 'sb', 'so', 'za', 'gs', 'es', 'lk', 'sd', 'sr', 'sj', 'sz', 'se', 'ch', 'sy', 'tj', 'tz', 'tl', 'tg', 'tk', 'to', 'tt', 'tn', 'tr', 'tm', 'tc', 'tv', 'ug', 'ua', 'ae', 'gb', 'us', 'um', 'uy', 'uz', 'vu', 've', 'vg', 'vi', 'wf', 'eh', 'ye', 'zm', 'zw'],
+      disableCountries: ['af', 'ax', 'al', 'dz', 'as', 'ad', 'ao', 'ai', 'aq', 'ag', 'am', 'aw', 'au', 'at', 'az', 'bs', 'bh', 'bd', 'bb', 'by', 'be', 'bz', 'bj', 'bm', 'bt', 'bo', 'ba', 'bw', 'bv', 'br', 'io', 'bn', 'bg', 'bf', 'bi', 'kh', 'cm', 'ca', 'cv', 'ky', 'cf', 'td', 'cl', 'cn', 'cx', 'cc', 'km', 'cg', 'cd', 'ck', 'cr', 'ci', 'hr', 'cy', 'cz', 'dk', 'dj', 'dm', 'do', 'sv', 'gq', 'er', 'ee', 'et', 'fk', 'fo', 'fj', 'fi', 'fr', 'gf', 'pf', 'tf', 'ga', 'gm', 'ge', 'gh', 'gi', 'gr', 'gl', 'gd', 'gp', 'gu', 'gt', 'gg', 'gn', 'gw', 'gy', 'ht', 'hm', 'va', 'hn', 'hk', 'hu', 'is', 'in', 'id', 'ir', 'iq', 'ie', 'im', 'il', 'it', 'jm', 'jp', 'je', 'jo', 'kz', 'ke', 'ki', 'kr', 'kw', 'kg', 'la', 'lv', 'ls', 'lr', 'ly', 'li', 'lt', 'lu', 'mo', 'mk', 'mg', 'mw', 'my', 'mv', 'ml', 'mt', 'mh', 'mq', 'mr', 'mu', 'yt', 'fm', 'md', 'mc', 'me', 'ms', 'ma', 'mz', 'mm', 'nr', 'np', 'nl', 'an', 'nc', 'nz', 'ni', 'ne', 'ng', 'nu', 'nf', 'mp', 'om', 'pk', 'pw', 'ps', 'pa', 'pg', 'py', 'pe', 'ph', 'pn', 'pl', 'pt', 'pr', 'qa', 're', 'ro', 'ru', 'rw', 'bl', 'sh', 'kn', 'lc', 'mf', 'pm', 'vc', 'ws', 'sm', 'st', 'sa', 'sn', 'sw', 'rs', 'sc', 'sl', 'sg', 'sk', 'si', 'sb', 'so', 'za', 'gs', 'es', 'lk', 'sd', 'sr', 'sj', 'sz', 'se', 'ch', 'sy', 'tj', 'tz', 'tl', 'tg', 'tk', 'to', 'tt', 'tn', 'tr', 'tm', 'tc', 'tv', 'ug', 'ua', 'ae', 'gb', 'us', 'um', 'uy', 'uz', 'vu', 've', 'vg', 'vi', 'wf', 'eh', 'ye', 'zm', 'zw'],
       individualCountrySettings: [
         {name: 'de', color: '#075c98'},
         {name: 'eg', color: '#075c98'},
@@ -63,40 +65,15 @@ $(document).ready(function () {
         {name: 'na', color: '#075c98'},
         {name: 'ar', color: '#075c98'},
         {name: 'cu', color: '#075c98'},
-        {name: 'vn', color: '#075c98'}
+        {name: 'vn', color: '#075c98'},
+        {name: 'ec', color: '#075c98'},
+        {name: 'co', color: '#075c98'},
+        {name: 'mx', color: '#075c98'}
       ]
     }, function () {
-      // setFlag('de')
-      // setFlag('th')
-      // setFlag('na')
     })
   }
 })
-/* function setFlag (country) {
-  var pos = $('svg path#' + country + ', svg text#' + country + ', svg rect#' + country).offset()
-  $('body').last().after('<img data-country="' + country + '" src="images/flag-' + country + '.png" style="left:' + pos.left + 'px;top:' + pos.top + 'px" class="flag-marker" onclick="loadVideo(\'' + country + '\')"/>')
-} */
-/* function loadVideo (country) {
-  let video = ''
-  switch (country) {
-    case 'de': video = 'videos/1_Kirschbaum_Deutschland.mp4'; break
-    case 'eg': video = 'videos/2_Grosser_Bruder_Aegypten.mp4'; break
-    case 'no': video = 'videos/3_Nicht_Rosa_Norwegen.mp4'; break
-    case 'lb': video = 'videos/4-Geschlagen_Syrien.mp4'; break
-    case 'th': video = 'videos/7_Ballspielen_verboten_Thailand.mp4'; break
-    case 'tw': video = 'videos/11_Elternstreit_Taiwan.mp4'; break
-    case 'mn': video = 'videos/9-Sleepy_Mongolei.mp4'; break
-    case 'na': video = 'videos/8_Allein_im_Busch_Namibia.mp4'; break
-    case 'ar': video = 'videos/13-Stromausfall_Argentinien.mp4'; break
-    default: video = ''; break
-  }
-  if (video !== '') {
-    $('.overlay').show()
-    $('.custom-modal').show()
-    $('.custom-modal video source').attr('src', video)
-    $('.custom-modal video')[0].load()
-  }
-} */
 function scrollTo (country) {
   let element = ''
   switch (country) {
@@ -111,9 +88,13 @@ function scrollTo (country) {
     case 'ar': element = 'stromausfall'; break
     case 'cu': element = 'meer'; break
     case 'vn': element = 'omelette'; break
+    case 'ec': element = ''; alert('Coming Soon...'); break
+    case 'co': element = ''; alert('Coming Soon...'); break
+    case 'mx': element = ''; alert('Coming Soon...'); break
     default: element = ''; break
   }
   if (element !== '') {
+    history.pushState({}, 'Starke Geschichten', '#' + element)
     $('html, body').animate({ scrollTop: ($('#' + element).offset().top)}, 'slow')
   }
 }
