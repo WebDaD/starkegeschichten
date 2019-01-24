a4074df169cb8d6f49b05620b8f0367b014db863
Dominik Sigmund <dominik.sigmund@br.de>
2019-01-24 14:27:30 +0100

    coming soon

--- TODO.md
+++ TODO.md
@@ -5,3 +5,5 @@
 * Schrifttyp: Im Moment noch sehr nüchtern. Lass uns mal probieren, ob wir eine freundlichere, rundere Schrift finden.
 * rebuild in pug!
 * Auch zu starke Diskrepanz zwischen Größe der Überschrift(en) und Größer der Textschrift, das sollten wir ausgewogener darstellen.
+
+* Coming Soon: Ja, dass soll quasi als Tooltip bei der Maus kommen. Hat neulich nicht geklappt, deshalb der Kasten als Zwischenlösung
--- js/index.js
+++ js/index.js
@@ -53,6 +53,14 @@ $(document).ready(function () {
       click: function (country) {
         scrollTo(country)
       },
+      hover: function (country) {
+        switch (country) {
+          case 'ec': $('#coming-soon').show(); break
+          case 'co': $('#coming-soon').show(); break
+          case 'mx': $('#coming-soon').show(); break
+          default: $('#coming-soon').hide(); break
+        }
+      },
       disableCountries: ['af', 'ax', 'al', 'dz', 'as', 'ad', 'ao', 'ai', 'aq', 'ag', 'am', 'aw', 'au', 'at', 'az', 'bs', 'bh', 'bd', 'bb', 'by', 'be', 'bz', 'bj', 'bm', 'bt', 'bo', 'ba', 'bw', 'bv', 'br', 'io', 'bn', 'bg', 'bf', 'bi', 'kh', 'cm', 'ca', 'cv', 'ky', 'cf', 'td', 'cl', 'cn', 'cx', 'cc', 'km', 'cg', 'cd', 'ck', 'cr', 'ci', 'hr', 'cy', 'cz', 'dk', 'dj', 'dm', 'do', 'sv', 'gq', 'er', 'ee', 'et', 'fk', 'fo', 'fj', 'fi', 'fr', 'gf', 'pf', 'tf', 'ga', 'gm', 'ge', 'gh', 'gi', 'gr', 'gl', 'gd', 'gp', 'gu', 'gt', 'gg', 'gn', 'gw', 'gy', 'ht', 'hm', 'va', 'hn', 'hk', 'hu', 'is', 'in', 'id', 'ir', 'iq', 'ie', 'im', 'il', 'it', 'jm', 'jp', 'je', 'jo', 'kz', 'ke', 'ki', 'kr', 'kw', 'kg', 'la', 'lv', 'ls', 'lr', 'ly', 'li', 'lt', 'lu', 'mo', 'mk', 'mg', 'mw', 'my', 'mv', 'ml', 'mt', 'mh', 'mq', 'mr', 'mu', 'yt', 'fm', 'md', 'mc', 'me', 'ms', 'ma', 'mz', 'mm', 'nr', 'np', 'nl', 'an', 'nc', 'nz', 'ni', 'ne', 'ng', 'nu', 'nf', 'mp', 'om', 'pk', 'pw', 'ps', 'pa', 'pg', 'py', 'pe', 'ph', 'pn', 'pl', 'pt', 'pr', 'qa', 're', 'ro', 'ru', 'rw', 'bl', 'sh', 'kn', 'lc', 'mf', 'pm', 'vc', 'ws', 'sm', 'st', 'sa', 'sn', 'sw', 'rs', 'sc', 'sl', 'sg', 'sk', 'si', 'sb', 'so', 'za', 'gs', 'es', 'lk', 'sd', 'sr', 'sj', 'sz', 'se', 'ch', 'sy', 'tj', 'tz', 'tl', 'tg', 'tk', 'to', 'tt', 'tn', 'tr', 'tm', 'tc', 'tv', 'ug', 'ua', 'ae', 'gb', 'us', 'um', 'uy', 'uz', 'vu', 've', 'vg', 'vi', 'wf', 'eh', 'ye', 'zm', 'zw'],
       individualCountrySettings: [
         {name: 'de', color: '#075c98'},
@@ -71,6 +79,10 @@ $(document).ready(function () {
         {name: 'mx', color: '#075c98'}
       ]
     }, function () {
+      // Coming Soon Stuff
+      $('g#ec').attr('title', 'Coming Soon...')
+      // $('g#co').attr('title', 'Coming Soon...')
+      $('g#mx').attr('title', 'Coming Soon...')
     })
   }
 })
@@ -88,14 +100,16 @@ function scrollTo (country) {
     case 'ar': element = 'stromausfall'; break
     case 'cu': element = 'meer'; break
     case 'vn': element = 'omelette'; break
-    case 'ec': element = ''; alert('Coming Soon...'); break
-    case 'co': element = ''; alert('Coming Soon...'); break
-    case 'mx': element = ''; alert('Coming Soon...'); break
+    case 'ec': element = ''; break
+    case 'co': element = ''; break
+    case 'mx': element = ''; break
     default: element = ''; break
   }
   if (element !== '') {
-    history.pushState({}, 'Starke Geschichten', '#' + element)
+    history.pushState({}, 'Starke Geschichten für starke Kinder', '#' + element)
     $('html, body').animate({ scrollTop: ($('#' + element).offset().top)}, 'slow')
+    $('.card').removeClass('active-card')
+    $('#' + element).addClass('active-card')
   }
 }
 function showSlides (n) {
