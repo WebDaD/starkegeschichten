83d9373ea7566a5b02754c8905b4dd927794d9de
Dominik Sigmund <dominik.sigmund@br.de>
2019-02-07 13:37:17 +0100

    Fix Tooltip

--- TODO.md
+++ TODO.md
@@ -6,6 +6,5 @@
 * rebuild in pug!
 * Auch zu starke Diskrepanz zwischen Größe der Überschrift(en) und Größer der Textschrift, das sollten wir ausgewogener darstellen.
 
-* Coming Soon: Ja, dass soll quasi als Tooltip bei der Maus kommen. Hat neulich nicht geklappt, deshalb der Kasten als Zwischenlösung
 
 * Die Titelangaben in den Diashow-Bildern arbeiten nicht mit: Wenn man die Seite kleiner zieht, ragen sie aus dem Bild heraus. Und sie sind auch nicht mittig, auch bei voller Bildschirmansicht nicht. Kann man die responsiver gestalten?
--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 /* global $, history */
 var slideIndex = 1
+var mouseX
+var mouseY
+$(document).mousemove(function (e) {
+  mouseX = e.pageX
+  mouseY = e.pageY
+})
 $(document).ready(function () {
   // wrap card title after 30 chars
 
@@ -61,14 +67,6 @@ $(document).ready(function () {
       click: function (country) {
         scrollTo(country)
       },
-      hover: function (country) {
-        switch (country) {
-          case 'ec': $('#coming-soon').show(); break
-          case 'co': $('#coming-soon').show(); break
-          case 'mx': $('#coming-soon').show(); break
-          default: $('#coming-soon').hide(); break
-        }
-      },
       disableCountries: ['af', 'ax', 'al', 'dz', 'as', 'ad', 'ao', 'ai', 'aq', 'ag', 'am', 'aw', 'au', 'at', 'az', 'bs', 'bh', 'bd', 'bb', 'by', 'be', 'bz', 'bj', 'bm', 'bt', 'bo', 'ba', 'bw', 'bv', 'br', 'io', 'bn', 'bg', 'bf', 'bi', 'kh', 'cm', 'ca', 'cv', 'ky', 'cf', 'td', 'cl', 'cn', 'cx', 'cc', 'km', 'cg', 'cd', 'ck', 'cr', 'ci', 'hr', 'cy', 'cz', 'dk', 'dj', 'dm', 'do', 'sv', 'gq', 'er', 'ee', 'et', 'fk', 'fo', 'fj', 'fi', 'fr', 'gf', 'pf', 'tf', 'ga', 'gm', 'ge', 'gh', 'gi', 'gr', 'gl', 'gd', 'gp', 'gu', 'gt', 'gg', 'gn', 'gw', 'gy', 'ht', 'hm', 'va', 'hn', 'hk', 'hu', 'is', 'in', 'id', 'ir', 'iq', 'ie', 'im', 'il', 'it', 'jm', 'jp', 'je', 'jo', 'kz', 'ke', 'ki', 'kr', 'kw', 'kg', 'la', 'lv', 'ls', 'lr', 'ly', 'li', 'lt', 'lu', 'mo', 'mk', 'mg', 'mw', 'my', 'mv', 'ml', 'mt', 'mh', 'mq', 'mr', 'mu', 'yt', 'fm', 'md', 'mc', 'me', 'ms', 'ma', 'mz', 'mm', 'nr', 'np', 'nl', 'an', 'nc', 'nz', 'ni', 'ne', 'ng', 'nu', 'nf', 'mp', 'om', 'pk', 'pw', 'ps', 'pa', 'pg', 'py', 'pe', 'ph', 'pn', 'pl', 'pt', 'pr', 'qa', 're', 'ro', 'ru', 'rw', 'bl', 'sh', 'kn', 'lc', 'mf', 'pm', 'vc', 'ws', 'sm', 'st', 'sa', 'sn', 'sw', 'rs', 'sc', 'sl', 'sg', 'sk', 'si', 'sb', 'so', 'za', 'gs', 'es', 'lk', 'sd', 'sr', 'sj', 'sz', 'se', 'ch', 'sy', 'tj', 'tz', 'tl', 'tg', 'tk', 'to', 'tt', 'tn', 'tr', 'tm', 'tc', 'tv', 'ug', 'ua', 'ae', 'gb', 'us', 'um', 'uy', 'uz', 'vu', 've', 'vg', 'vi', 'wf', 'eh', 'ye', 'zm', 'zw'],
       individualCountrySettings: [
         {name: 'de', color: '#075c98'},
@@ -88,9 +86,21 @@ $(document).ready(function () {
       ]
     }, function () {
       // Coming Soon Stuff
-      $('g#ec').attr('title', 'Coming Soon...')
-      // $('g#co').attr('title', 'Coming Soon...')
-      $('g#mx').attr('title', 'Coming Soon...')
+      $('g#ec').addClass('hover')
+      $('g#co').addClass('hover')
+      $('g#mx').addClass('hover')
+      $('.hover').mouseover(function () {
+        // show tooltip
+        $('#coming-soon').stop(false, true).fadeIn(1)
+      
+        // position tooltip relative to mouse coordinates
+        $(this).mousemove(function () {
+          $('#coming-soon').css({'top': mouseY - 100, 'left': mouseX - 100})
+        })
+      }).mouseout(function () {
+      // hide tooltip
+        $('#coming-soon').stop(false, true).fadeOut(1)
+      })
     })
   }
 })
@@ -138,7 +148,7 @@ $.fn.hasOverflown = function () {
   .css('font-family', this.css('font-family'))
   .css('font-size', this.css('font-size'))
   .css('font-weight', this.css('font-weight')).appendTo('body')
-  res = (cont.width() >  this.width())
+  res = (cont.width() > this.width())
   cont.remove()
   return res
 }
